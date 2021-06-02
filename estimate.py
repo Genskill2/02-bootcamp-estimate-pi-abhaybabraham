34cc8028fde36e852e777d2d000193ad1e8946d9
abhaybabraham <abhaybabraham@gmail.com>
2021-06-02 10:04:33 +0530

    new

--- estimate.py
+++ estimate.py
@@ -6,7 +6,6 @@ def wallis(n):
     pi=2;
     for i in range(1,n+1):
         pi*=(4*i**2)/(4*i**2-1)
-    print(pi)
     return pi
         
 def monte_carlo(n):
@@ -17,7 +16,6 @@ def monte_carlo(n):
         d=(x**2+y**2)**0.5
         if d<=1:
             c+=1
-    print(4*(c/p))
     return 4*(c/p)
     
 
